eafa45a42d8301cb1ea6c6c924f77e8a58c8a73f
Caio Appelt Rezende <caioarezende@outlook.com>
2024-02-28 00:31:56 -0300

    feat: initial config with heap

---
+++ src/classes/heap.ts
@@ -0,0 +1,118 @@
+abstract class Heap<T> {
+  _heap: T[] = [];
+
+  root(): T | null {
+    return this._heap.length > 0 ? this._heap[0] : null;
+  }
+
+  add(element: T) {
+    this._heap.push(element);
+
+    this.bubble();
+  }
+
+  protected bubble() {
+    let start = this._heap.length - 1;
+
+    while (start !== null) {
+      let parent = this.parent(start);
+      if (parent === null) {
+        break;
+      }
+
+      if (this.compare(this._heap[start], this._heap[parent])) {
+        this.swap(start, parent);
+        start = parent;
+      } else {
+        break;
+      }
+    }
+  }
+
+  remove(): T | null {
+    if (this._heap.length === 0) return null;
+
+    const element = this._heap[0];
+
+    this.sink();
+
+    return element;
+  }
+
+  protected sink() {
+    let start = 0;
+
+    while (start < this._heap.length) {
+      const left = this.left(start);
+      const right = this.right(start);
+
+      if (left !== null && right === null) {
+        this.swap(start, left);
+        this._heap.pop();
+        break;
+      } else if (left !== null && right !== null) {
+        if (this.compare(this._heap[left], this._heap[right])) {
+          this.swap(start, left);
+          start = left;
+        } else {
+          this.swap(start, right);
+          start = right;
+        }
+      } else {
+        this.swap(start, this._heap.length - 1);
+        this._heap.pop();
+        break;
+      }
+    }
+  }
+
+  protected left(index: number): number | null {
+    const child = index * 2 + 1;
+
+    if (child < this._heap.length) {
+      return child;
+    }
+
+    return null;
+  }
+
+  protected right(index: number): number | null {
+    const child = index * 2 + 2;
+
+    if (child < this._heap.length) {
+      return child;
+    }
+
+    return null;
+  }
+
+  protected parent(index: number): number | null {
+    if (index === 0) {
+      return null;
+    }
+
+    if (index % 2 === 0) {
+      return index / 2 - 1;
+    } else {
+      return (index - 1) / 2;
+    }
+  }
+
+  protected swap(a: number, b: number) {
+    [this._heap[a], this._heap[b]] = [this._heap[b], this._heap[a]];
+  }
+
+  protected abstract compare(a: T, b: T): boolean;
+}
+
+export class MaxHeap<T> extends Heap<T> {
+  compare(a: T, b: T): boolean {
+    return a > b;
+  }
+}
+
+export class MinHeap<T> extends Heap<T> {
+  compare(a: T, b: T): boolean {
+    return a < b;
+  }
+}
---
+++ src/tests/heap.test.ts
@@ -0,0 +1,69 @@
+import { MaxHeap, MinHeap } from "classes/heap";
+import assert from "node:assert";
+import { describe, it } from "node:test";
+
+describe("heap test - basic", () => {
+  it("should get min", () => {
+    const heap = new MinHeap<number>();
+    heap.add(1);
+    heap.add(2);
+    heap.add(3);
+    heap.add(0);
+
+    assert.strictEqual(heap.root(), 0);
+  });
+
+  it("should get max", () => {
+    const heap = new MaxHeap<number>();
+    heap.add(1);
+    heap.add(2);
+    heap.add(3);
+    heap.add(0);
+
+    assert.strictEqual(heap.root(), 3);
+  });
+});
+
+describe("heap test - sort", () => {
+  it("order list desc", () => {
+    const heap = new MaxHeap<number>();
+    heap.add(100);
+    heap.add(10);
+    heap.add(1);
+    heap.add(0);
+    heap.add(-100);
+    heap.add(-1000);
+    heap.add(50);
+    heap.add(-50);
+
+    let sorted: number[] = [];
+    let element = heap.remove();
+    while (element !== null) {
+      sorted.push(element!);
+      element = heap.remove();
+    }
+
+    assert.deepStrictEqual(sorted, [100, 50, 10, 1, 0, -50, -100, -1000]);
+  });
+
+  it("order list asc", () => {
+    const heap = new MinHeap<number>();
+    heap.add(100);
+    heap.add(10);
+    heap.add(1);
+    heap.add(0);
+    heap.add(-100);
+    heap.add(-1000);
+    heap.add(50);
+    heap.add(-50);
+
+    let sorted: number[] = [];
+    let element = heap.remove();
+    while (element !== null) {
+      sorted.push(element!);
+      element = heap.remove();
+    }
+
+    assert.deepStrictEqual(sorted, [-1000, -100, -50, 0, 1, 10, 50, 100]);
+  });
+});
